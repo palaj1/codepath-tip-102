405ba7aa55c1a684508a73a3995370622cde6dd0
palaj1 <194732922+palaj1@users.noreply.github.com>
2025-02-22 18:59:44 -0800

    Unit 1: Problem 6 Double Trouble

---
+++ unit_1/s1/double_trouble_p6.py
@@ -0,0 +1,11 @@
+"""
+    Prompt:
+
+    Help Winnie the Pooh double his honey! Write a function doubled() that
+    accepts a list of integers hunny_hars as a parameter and multiplies each
+    element in the list by two. Return the doubled list
+"""
+
+def doubled(hunny_jars):
+    return list(map(lambda x: x * 2, hunny_jars))
+

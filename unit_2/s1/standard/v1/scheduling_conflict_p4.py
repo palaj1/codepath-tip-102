f199cc74370c58788fd273bfcd72109d48e0c4d6
palaj1 <194732922+palaj1@users.noreply.github.com>
2025-02-26 18:39:16 -0800

    Unit 2: Problem 4 Scheduling Conflict

---
+++ unit_2/s1/standard/v1/scheduling_conflict_p4.py
@@ -0,0 +1,30 @@
+"""
+    Prompt:
+
+    Demand for your festival has exceeded expectations, so you're expanding the festival to span two
+    different venues. Some artists will perform both venues, while others will perform at just one.
+    To ensure that there are no scheduling conflicts, implement a function identify_conflicts()
+    that accepts two dictionaries venue1_schedule and venue2_schedule each mapping the artists playing 
+    at the venue to their set times.
+
+    Return a dictionary containing the key-value pairs that are the same in each schedule
+"""
+
+def identify_conflicts(venue1_schedule: dict, venue2_schedule: dict):
+    return {artist: time for artist, time in venue1_schedule.items() if venue2_schedule.get(artist) == time}
+
+venue1_schedule = {
+    "Stromae": "9:00 PM",
+    "Janelle Monáe": "8:00 PM",
+    "HARDY": "7:00 PM",
+    "Bruce Springsteen": "6:00 PM"
+}
+
+venue2_schedule = {
+    "Stromae": "9:00 PM",
+    "Janelle Monáe": "10:30 PM",
+    "HARDY": "7:00 PM",
+    "Wizkid": "6:00 PM"
+}
+
+print(identify_conflicts(venue1_schedule, venue2_schedule))

e8f57600a957603f93c39caf4f12a23ca01ca1d4
palaj1 <194732922+palaj1@users.noreply.github.com>
2025-03-06 13:39:05 -0800

    Unit 3: (s1) Problem 3 Check Symmetry in Post Titles

---
+++ unit_3/s1/standard/v1/check_symmetry_in_post_titles_p3.py
@@ -0,0 +1,13 @@
+"""
+    Prompt:
+
+    As part of a new feature on your social media platform. you want to highlight post titles that are 
+    symmetrical, meaning they read the same forwards and backwards when ignoring spaces, punctuation, and
+    case.
+    
+    Given a post title as a string, use a post title as a string, use a new algorithmic technique the two
+    pointer method to determine if the title is symmetrical.
+"""
+
+def is_symmetrical_title(title: str):
+    
